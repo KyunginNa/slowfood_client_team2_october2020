fc06d105eca37deb9404232203f1ce77cd0a3997
Kyungin Na <nakyungin93@gmail.com>
2021-01-02 00:00:05 +0100

    Adding fixture files for when users add products to their order Adding a test case for adding the second product to order in userCanAddProductsToOrder.feature.js Adding a logic for adding the second product in addToOrder function

--- cypress/integration/userCanAddProductsToOrder.feature.js
+++ cypress/integration/userCanAddProductsToOrder.feature.js
@@ -1,4 +1,4 @@
-describe("User can add a product to their order", () => {
+describe("User can add products to their order", () => {
   beforeEach(() => {
     cy.server();
     cy.route({
@@ -16,10 +16,21 @@ describe("User can add a product to their order", () => {
     });
     cy.route({
       method: "POST",
-      url: "http://localhost:3000/api/orders",
-      response: {
-        message: "The product has been added to your order successfully.",
+      url: "http://localhost:3000/api/auth/sign_in",
+      response: "fixture:successful_login.json",
+      headers: {
+        uid: "test@test.com",
       },
+    });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/orders",
+      response: "fixture:first_product_added_to_order.json",
+    });
+    cy.route({
+      method: "PUT",
+      url: "http://localhost:3000/api/orders/**",
+      response: "fixture:second_product_added_to_order.json",
     });
     cy.visit("/");
     cy.get("[data-cy='input-email']").type("test@test.com");
@@ -27,11 +38,33 @@ describe("User can add a product to their order", () => {
     cy.get("[data-cy='input-password-confirmation']").type("password");
     cy.get("[data-cy='btn-submit']").click();
   });
-  it("user gets a confirmation message after adding a product to order", () => {
-    cy.get("[data-cy='btn-add-product1']").click();
-    cy.get("[data-cy='order-message']").should(
-      "contain",
-      "The product has been added to your order successfully."
-    );
+
+  describe("when clicking the Add To Order button of the first product ", () => {
+    it("user gets a confirmation message after adding a product to order", () => {
+      cy.get("[data-cy='btn-add-product1']").click();
+      cy.get("[data-cy='order-message']").should(
+        "contain",
+        "The product has been added to your order successfully. (1 × Mandu)"
+      );
+      cy.get("[data-cy='items-count-message']").should(
+        "contain",
+        "You have 1 item in your order."
+      );
+    });
+  });
+
+  describe("when clicking the Add To Order button of the second product ", () => {
+    it("user gets a confirmation message after adding a product to order", () => {
+      cy.get("[data-cy='btn-add-product1']").click();
+      cy.get("[data-cy='btn-add-product2']").click();
+      cy.get("[data-cy='order-message']").should(
+        "contain",
+        "The product has been added to your order successfully. (1 × Bibimbap)"
+      );
+      cy.get("[data-cy='items-count-message']").should(
+        "contain",
+        "You have 2 items in your order."
+      );
+    });
   });
 });
--- src/components/DisplayProducts.jsx
+++ src/components/DisplayProducts.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 const DisplayProducts = () => {
   const [orderMessage, setOrderMessasge] = useState()
+  const [orderID, setOrderID] = useState()
+  const [itemsCountMessage, setItemsCountMessage] = useState()
   const dispatch = useDispatch()
   const products = useSelector(state => state.products)
   const credentials = useSelector(state => state.credentials)
@@ -13,13 +15,24 @@ const DisplayProducts = () => {
   }
   useEffect(getProducts, [])
 
-  const addToOrder = async (productID) => {
-    let response = await axios.post(
-      "http://localhost:3000/api/orders",
-      { product_id: productID },
-      { headers: credentials },
-    )
-    setOrderMessasge(response.data.message)
+  const addToOrder = async (productID, productName) => {
+    if (orderID) {
+      let response = await axios.put(`http://localhost:3000/api/orders/${orderID}`,
+        { product_id: productID },
+        { headers: credentials },
+      )
+      setItemsCountMessage(`You have ${response.data.items.length} items in your order.`)
+      setOrderMessasge(`${response.data.message} (1 × ${productName})`)
+    } else {
+      let response = await axios.post(
+        "http://localhost:3000/api/orders",
+        { product_id: productID },
+        { headers: credentials },
+      )
+      setOrderID(response.data.items[0].order_id)
+      setItemsCountMessage(`You have 1 item in your order.`)
+      setOrderMessasge(`${response.data.message} (1 × ${productName})`)
+    }
   }
 
   return (
@@ -36,13 +49,15 @@ const DisplayProducts = () => {
               {product.price}
               <button
                 data-cy={`btn-add-product${product.id}`}
-                onClick={() => addToOrder(product.id)}
+                onClick={() => addToOrder(product.id, product.name)}
               >Add To Order
-          </button>
-            </div>)
+                </button>
+            </div>
+          )
         })}
       </div >
       <p data-cy="order-message">{orderMessage}</p>
+      <p data-cy="items-count-message">{itemsCountMessage}</p>
     </>
   )
 }
