250dc1e7c1cdf3af6aeae66aff4ef9b66c4c1f88
Kyungin Na <nakyungin93@gmail.com>
2021-01-01 04:04:20 +0100

    Adding logic to make registration form invisible when user is logged in

--- src/components/UserRegistration.jsx
+++ src/components/UserRegistration.jsx
@@ -22,34 +22,36 @@ const UserRegistration = (e) => {
 
   return (
     <>
-      <form>
-        <input
-          type="email"
-          name="email"
-          data-cy="input-email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          name="password"
-          data-cy="input-password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-          type="password"
-          name="password_confirmation"
-          data-cy="input-password-confirmation"
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-        />
-        <input
-          type="submit"
-          value="Submit"
-          data-cy="btn-submit"
-          onClick={createAccount}
-        />
-      </form>
-      {userID &&
-        <p data-cy="message">Welcome, {userID}!</p>
+      {!userID ? (
+        <form>
+          <input
+            type="email"
+            name="email"
+            data-cy="input-email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            data-cy="input-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password_confirmation"
+            data-cy="input-password-confirmation"
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+          <input
+            type="submit"
+            value="Submit"
+            data-cy="btn-submit"
+            onClick={createAccount}
+          />
+        </form>
+      ) : (
+          <p data-cy="message">Welcome, {userID}!</p>
+        )
       }
     </>
   )
