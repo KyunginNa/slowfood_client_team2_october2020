dd167d96f4dd8578ad045db92c1f498882aeecfc
Kyungin Na <nakyungin93@gmail.com>
2021-01-01 03:57:23 +0100

    Creating a test to check if visitors can create an acoount Creating a fixture file for user registration Creaitng UserRegistration component Adding SET_UID action in rootReducer.js

---
+++ cypress/integration/visitorCanRegister.feature.js
@@ -0,0 +1,27 @@
+describe("Visitors can create an account", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/products",
+      response: "fixture:products_index.json",
+    });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth",
+      response: "fixture:registration.json",
+      headers: {
+        uid: "test@test.com",
+      },
+    });
+    cy.visit("/");
+  });
+
+  it("successfully", () => {
+    cy.get("[data-cy='input-email']").type("test@test.com");
+    cy.get("[data-cy='input-password']").type("password");
+    cy.get("[data-cy='input-password-confirmation']").type("password");
+    cy.get("[data-cy='btn-submit']").click();
+    cy.get("[data-cy='message']").should("contain", "Welcome, test@test.com!");
+  });
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DisplayProducts from './components/DisplayProducts'
+import UserRegistration from './components/UserRegistration'
 
 const App = () => {
   return (
     <>
       <h1>K-FOOD</h1>
+      <UserRegistration />
       <DisplayProducts />
     </>
   )
