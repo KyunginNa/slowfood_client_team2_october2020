00f7fbeff68ede12cbce85050425b84caf11efed
Kyungin Na <nakyungin93@gmail.com>
2021-01-02 23:35:29 +0100

    Adding and configuring react-stripe-elements Creating a test to check if users can check out thier order

---
+++ cypress/integration/userCanCheckOut.feature.js
@@ -0,0 +1,90 @@
+describe("User can check out thier order", () => {
+  before(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/products",
+      response: "fixture:products_index.json",
+    });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth",
+      response: "fixture:registration.json",
+      headers: {
+        uid: "test@test.com",
+      },
+    });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth/sign_in",
+      response: "fixture:successful_login.json",
+      headers: {
+        uid: "test@test.com",
+      },
+    });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/orders",
+      response: "fixture:first_product_added_to_order.json",
+    });
+    cy.route({
+      method: "PUT",
+      url: "http://localhost:3000/api/orders/**",
+      response: "fixture:second_product_added_to_order.json",
+    });
+    cy.visit("/");
+    cy.get("[data-cy='input-email']").type("test@test.com");
+    cy.get("[data-cy='input-password']").type("password");
+    cy.get("[data-cy='input-password-confirmation']").type("password");
+    cy.get("[data-cy='btn-submit']").click();
+    cy.get("[data-cy='btn-add-product1']").click();
+    cy.get("[data-cy='btn-add-product2']").click();
+    cy.get("[data-cy='btn-view-order']").click();
+  });
+  it("by filling in valid credit card information", () => {
+    cy.route({
+      method: "PUT",
+      url: "http://localhost:3000/api/orders/**",
+      response: { finalized: true },
+    });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/payments",
+      response: {
+        message: "Thank you! Your order will be ready in 20 minutes.",
+      },
+    });
+
+    cy.get("[data-cy='btn-confirm-order']").click();
+    cy.get("[data-cy='payment-form']").within(() => {
+      cy.get("[data-cy='card-number']").within(() => {
+        cy.wait(1000);
+        cy.get('iframe[name^="__privateStripeFrame"]').then(($iframe) => {
+          const $body = $iframe.contents().find("body");
+          cy.wrap($body)
+            .find('input[name="cardnumber"]')
+            .type("4242424242424242", { delay: 10 });
+        });
+      });
+      cy.get("[data-cy='card-expiry']").within(() => {
+        cy.get('iframe[name^="__privateStripeFrame"]').then(($iframe) => {
+          const $body = $iframe.contents().find("body");
+          cy.wrap($body)
+            .find('input[name="exp-date"]')
+            .type("0224", { delay: 10 });
+        });
+      });
+      cy.get("[data-cy='card-cvc']").within(() => {
+        cy.get('iframe[name^="__privateStripeFrame"]').then(($iframe) => {
+          const $body = $iframe.contents().find("body");
+          cy.wrap($body).find('input[name="cvc"]').type("424", { delay: 10 });
+        });
+      });
+      cy.get("[data-cy='btn-payment']").click();
+    });
+    cy.get("[data-cy='payment-message']").should(
+      "contain",
+      "Thank you! Your order will be ready in 20 minutes."
+    );
+  });
+});
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import configureStore from "./state/store/configureStore";
 import { Provider } from "react-redux";
+import { StripeProvider } from "react-stripe-elements";
 
 const store = configureStore();
 window.store = store;
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StripeProvider apiKey="pk_test_51HuxwPB4iQLxMzwRoYvwIg9YWiZPJbCCNQvYgR53ye90XKjtqHZGIwJBbnOwoM0HRPf1YrQ0J44YJlUviKfXUTIU00HdNTkvl4">
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StripeProvider>,
   document.getElementById("root")
 );
 
